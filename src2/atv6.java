3599faeb8931f7ae8cc48302776e0d874544ee34
V1ctorRodrigues <victor.rodrigueslima@ufrpe.br>
2025-09-26 00:05:03 -0300

    Atividade 2 - Exercício 6

--- src2/atv6.java
+++ src2/atv6.java
@@ -1,7 +1,6 @@
 public class atv6 {
     public static void main(String[] args) {
         String [] nomes = {"Ana", "Bruno", "Alê", "Aline", "Clara"};
-        String a = "a";
 
         for (String nome : nomes){
             if(nome.startsWith("A")){
