c354ed63a8852676c7a2ab8217192dc0a1bf1cc2
V1ctorRodrigues <victor.rodrigueslima@ufrpe.br>
2025-11-03 11:43:39 -0300

    Atualização

--- Atividade 5/Exercicio1.java
+++ Atividade 5/Exercicio1.java
@@ -2,25 +2,6 @@ import java.util.ArrayList;
 
 public class Exercicio1 {
      public static void main(String[] args) {
-          ArrayList<String> produtos = new ArrayList<>();
-          int resposta = 1;
-          int respostaTeclado;
-          int removerItem;
-          String item;
-          while(true){
-               if(resposta == 0){
-                    break;
-               }
-               else if(resposta!=0){
-                    switch(respostaTeclado) {
-                         case 1:
-                         Produtos.mostrarLista(produtos);
-                         case 2:
-                         Produtos.adicionarItem(produtos, item);
-                         case 3: 
-                         Produtos.removerItem(produtos, removerItem);
-                    }
-               }
-          }
+          
      }
 }
--- Atividade 5/Produtos.java
+++ Atividade 5/Produtos.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Produtos{
 
@@ -14,6 +15,20 @@ public class Produtos{
 
     public static void removerItem(ArrayList<String> lista, int index){
         lista.remove(index);
+    }
+
+    public static void exibirMenu(){
+        boolean flag = true;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("1 - Mostrar lista 2 - Adicionar item 3 - Remover item 4 - Sair");
+        int respostaUsuario = sc.nextInt(); 
+
+        while(flag == true){
+            switch (respostaUsuario) {
+                case 1:
+                    Produtos.mostrarLista();
+            }
+        }
     }
 
 
